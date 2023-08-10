22a73832bcc304eb28cb47af425f17daa2d05bc1
MyronRodrigues-StreetDrone <myron.rodrigues@streetdrone.com>
2023-08-10 17:16:15 +0000

    cleanup cpu preprocessing

--- tools/voxelnext_utils/preprocess.py
+++ tools/voxelnext_utils/preprocess.py
@@ -204,35 +204,6 @@ class DataProcessor:
 
     return voxels, coordinates, num_points
 
-  def sample_points(self, points, config=None):
-    num_points = config.NUM_POINTS[self.mode]
-    if num_points == -1:
-      return points
-
-    if num_points < len(points):
-      pts_depth = np.linalg.norm(points[:, 0:3], axis=1)
-      pts_near_flag = pts_depth < 40.0
-      far_idxs_choice = np.where(pts_near_flag == 0)[0]
-      near_idxs = np.where(pts_near_flag == 1)[0]
-      choice = []
-      if num_points > len(far_idxs_choice):
-        near_idxs_choice = np.random.choice(
-            near_idxs, num_points - len(far_idxs_choice),
-            replace=False)
-        choice = np.concatenate((near_idxs_choice, far_idxs_choice), axis=0) \
-            if len(far_idxs_choice) > 0 else near_idxs_choice
-      else:
-        choice = np.arange(0, len(points), dtype=np.int32)
-        choice = np.random.choice(choice, num_points, replace=False)
-      np.random.shuffle(choice)
-    else:
-      choice = np.arange(0, len(points), dtype=np.int32)
-      if num_points > len(points):
-        extra_choice = np.random.choice(choice, num_points - len(points), replace=False)
-        choice = np.concatenate((choice, extra_choice), axis=0)
-      np.random.shuffle(choice)
-    return points[choice]
-
   def preprocess(self, points, use_lead_xyz):
     for dp_cfg in self.data_processor_cfg:
       if dp_cfg.NAME == 'mask_points_and_boxes_outside_range':
